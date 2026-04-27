9a5ed27bef352ea4445f2a0e8f73e51873d39c98
Anji <zam58mrp@i.softbank.jp>
2026-04-27 12:42:50 +0900

    add:様々なイベント処理の練習

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,51 @@ import Child from "./components/Child";
 const clickHandler = () => {
   alert("ボタンがクリックされました。");
 }
-
-const App = () => {
+const App=()=>{
   return (
     <>
-      <button onClick={clickHandler}>クリック</button>
-      <button>クリック</button>
+    <div>
+      <h3>コンソールを確認してください</h3>
+      <label>
+        入力値のイベント:
+        <input
+        type="text"
+        onChange={()=>console.log("onChange検知")}
+        // フォーカスを失ったときに検知
+        onBlur={()=>console.log("onBlur検知")}
+        onFocus={()=>console.log("onFocus検知")}
+        />
+      </label>
+    </div>
+
+    <div>
+      <label>
+        入力値を取得:
+        <input type="text" onChange={(e)=>console.log(e.target.value)}/>
+      </label>
+    </div>
+
+    <div
+      className="hover-event"
+      onMouseEnter={()=>console.log("カーソルが入ってきました。")}
+      onMouseLeave={()=>console.log("カーソルが出ていきました。")}
+      >
+        ホバーしてください
+    </div>
     </>
+
   )
 }
+
+// const App = () => {
+//   return (
+//     <>
+//     {/* clickHandler()とすると実行したものとみなされるので付けない */}
+//       <button onClick={clickHandler}>クリック</button>
+//       <button>クリック</button>
+//     </>
+//   )
+// }
 
 export default App;
 // const App = () => {
