26d55b7d806fddb5ded7c685ecd4c21de8523d88
Anji <zam58mrp@i.softbank.jp>
2026-04-27 13:08:38 +0900

    add :useStateを使った入力された値を表示する練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,61 @@
-import Child from "./components/Child";
+import { useState } from "react";
+import type{ChangeEvent  } from "react";
 
-const clickHandler = () => {
-  alert("ボタンがクリックされました。");
-}
-const App=()=>{
+const App = () => {
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <>
-    <div>
-      <h3>コンソールを確認してください</h3>
-      <label>
-        入力値のイベント:
-        <input
-        type="text"
-        onChange={()=>console.log("onChange検知")}
-        // フォーカスを失ったときに検知
-        onBlur={()=>console.log("onBlur検知")}
-        onFocus={()=>console.log("onFocus検知")}
-        />
-      </label>
-    </div>
-
-    <div>
-      <label>
-        入力値を取得:
-        <input type="text" onChange={(e)=>console.log(e.target.value)}/>
-      </label>
-    </div>
-
-    <div
-      className="hover-event"
-      onMouseEnter={()=>console.log("カーソルが入ってきました。")}
-      onMouseLeave={()=>console.log("カーソルが出ていきました。")}
-      >
-        ホバーしてください
-    </div>
+      <input type="text" onChange={handleChange} />
+      {" = "}
+      {value}
     </>
+  );
+};
 
-  )
-}
+export default App;
+// const clickHandler = () => {
+//   alert("ボタンがクリックされました。");
+// }
+// const App=()=>{
+//   return (
+//     <>
+//     <div>
+//       <h3>コンソールを確認してください</h3>
+//       <label>
+//         入力値のイベント:
+//         <input
+//         type="text"
+//         onChange={()=>console.log("onChange検知")}
+//         // フォーカスを失ったときに検知
+//         onBlur={()=>console.log("onBlur検知")}
+//         onFocus={()=>console.log("onFocus検知")}
+//         />
+//       </label>
+//     </div>
+
+//     <div>
+//       <label>
+//         入力値を取得:
+//         <input type="text" onChange={(e)=>console.log(e.target.value)}/>
+//       </label>
+//     </div>
+
+//     <div
+//       className="hover-event"
+//       onMouseEnter={()=>console.log("カーソルが入ってきました。")}
+//       onMouseLeave={()=>console.log("カーソルが出ていきました。")}
+//       >
+//         ホバーしてください
+//     </div>
+//     </>
+
+//   )
+// }
 
 // const App = () => {
 //   return (
@@ -48,8 +66,6 @@ const App=()=>{
 //     </>
 //   )
 // }
-
-export default App;
 // const App = () => {
 //   const hello = (arg: string): string => `Hello ${arg}`;
 
